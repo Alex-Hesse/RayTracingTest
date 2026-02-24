056a60ec4dc2580709690f45a07564c5886f5038
Alex <alex@fedora.fritz.box>
2026-02-24 21:18:15 +0100

    Chapter 11: Dielectrics

--- src/main.cpp
+++ src/main.cpp
@@ -19,22 +19,24 @@ int main() {
 
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_left   = make_shared<dielectric>(1.00 / 1.33);
+    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
     auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.49, material_bubble));
     world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     camera cam{};
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width  = 1200;
-    cam.samples_per_pixel = 500;
+    cam.image_width  = 400;
+    cam.samples_per_pixel = 100;
     cam.max_depth         = 50;
     cam.render(world);
 
     auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::clog << "Execution time: " << duration.count() << " microseconds" << std::endl;
+    std::clog << "Execution time: " << duration.count() << " milliseconds" << std::endl;
 }
