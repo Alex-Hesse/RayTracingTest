5067bda7f405d66215dca395824711238a71ac3a
Alex <alex@fedora.fritz.box>
2026-02-24 19:57:12 +0100

    Chapter 8: Antialiasing

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,7 @@ int main() {
     camera cam{};
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width  = 800;
+    cam.samples_per_pixel = 100;
     cam.render(world);
 
     auto end = std::chrono::steady_clock::now();
