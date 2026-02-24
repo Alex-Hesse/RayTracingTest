3a8605f4d2932d3b7469b547939f84b604bf571c
Alex <alex@fedora.fritz.box>
2026-02-24 20:29:31 +0100

    Chapter 9: Diffuse material

--- include/color.h
+++ include/color.h
@@ -8,6 +8,16 @@
 
 using color = vec3;
 
+/* gamma correction
+*/
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
+
 /* writes color rgb (0.0-1.0) to out as [0,255]
 *
 */
@@ -15,6 +25,11 @@ void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
